181ecf9d41881f263d1f282567486bc298ce670d
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-09-16 01:18:38 -0300

    navigator.serviceWorker

--- js/main.js
+++ js/main.js
@@ -74,3 +74,5 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
 });
+
+navigator.serviceWorker.register('./sw.js')
